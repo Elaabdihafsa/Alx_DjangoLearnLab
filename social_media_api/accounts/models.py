00f7a7ea6950481830e525f43b6f44b5c7b4a8d5
Elaabdi Hafsa <hafsaaelaabdi@gmail.com>
2025-12-14 16:56:28 +0100

    Rename User model to CustomUser and update fields

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,11 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager 
 
-# User model extended from AbstractUser
-class User(AbstractUser):
-    bio = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    following = models.ManyToManyField('self', blank=True, related_name='followed_by', symmetrical=False)
-    
-    def __str__(self):
-        return self.username
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following',
+        blank=True
+    )
